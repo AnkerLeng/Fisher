12bd825d6466c333abb9d8bbedb985153d513f9f
anker <gyyx084@gmail.com>
2019-02-28 12:05:16 +0800

    dir before

--- fisher.py
+++ fisher.py
@@ -1,6 +1,9 @@
-from flask import Flask, make_response
+import json
+
+from flask import Flask, make_response, jsonify
 
 from helper import is_isbn_or_key
+from yushu_book import YuShuBook
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,8 +16,15 @@ def search(q, page):
     page
     """
     isbn_or_key = is_isbn_or_key(q)
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    else:
+        result = YuShuBook.search_by_keyword(q)
+    return jsonify(result)
+    # return json.dumps(result), 200, {'content-type': 'application/json'}
 
 
 if __name__ == '__main__':
     # 生产环境 nginx + uwsgi
     app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=5000)
+ 
